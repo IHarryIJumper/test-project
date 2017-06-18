2534c9bf7c023eb4357fb5327e72075c3326d560
Andrey Menshikh <andrey@light-tecnologies.com>
2017-06-18 21:54:04 +0300

    API for requests to server

---
+++ client/src/scripts/api/api.js
@@ -0,0 +1,117 @@
+import Promise from 'bluebird';
+
+import Request from './requestHelper/request.js';
+
+
+//employees
+
+const getEmployees = () => {
+	return new Promise((resolve, reject) => {
+		Request.get('employee', (response) => {
+			let data = [];
+			try {
+				if (typeof response === 'string') {
+					data = JSON.parse(response);
+				}
+			} catch (e) {
+				console.error('GET employees data parse failed', 'Data:', response);
+			}
+			resolve(data);
+		}, (error) => {
+			reject(error);
+		})
+	});
+};
+
+const addEmployee = (data) => {
+	return new Promise((resolve, reject) => {
+		Request.post('employee', data, (response) => {
+			resolve(response);
+		}, (error) => {
+			reject(error);
+		})
+	});
+};
+
+const updateEmployee = (id, data) => {
+	return new Promise((resolve, reject) => {
+		Request.put('employee', id, data, (response) => {
+			resolve(response);
+		}, (error) => {
+			reject(error);
+		})
+	});
+};
+
+const deleteEmployee = (id, data) => {
+	return new Promise((resolve, reject) => {
+		Request.delete('employee', id, (response) => {
+			resolve(response);
+		}, (error) => {
+			reject(error);
+		})
+	});
+};
+
+
+//departments
+const getDepartments = () => {
+	return new Promise((resolve, reject) => {
+		Request.get('employee', (response) => {
+			let data = [];
+			try {
+				if (typeof response === 'string') {
+					data = JSON.parse(response);
+				}
+			} catch (e) {
+				console.error('GET employees data parse failed', 'Data:', response);
+			}
+			resolve(data);
+		}, (error) => {
+			reject(error);
+		})
+	});
+};
+
+const addDepartment = (data) => {
+	return new Promise((resolve, reject) => {
+		Request.post('employee', data, (response) => {
+			resolve(response);
+		}, (error) => {
+			reject(error);
+		})
+	});
+};
+
+const updateDepartment = (id, data) => {
+	return new Promise((resolve, reject) => {
+		Request.put('employee', id, data, (response) => {
+			resolve(response);
+		}, (error) => {
+			reject(error);
+		})
+	});
+};
+
+const deleteDepartment = (id, data) => {
+	return new Promise((resolve, reject) => {
+		Request.delete('employee', id, (response) => {
+			resolve(response);
+		}, (error) => {
+			reject(error);
+		})
+	});
+};
+
+const API = {
+	getEmployees,
+	addEmployee,
+	updateEmployee,
+	deleteEmployee,
+	getDepartments,
+	addDepartment,
+	updateDepartment,
+	deleteDepartment
+};
+
+export default API;
