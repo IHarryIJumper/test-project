d0509d38af7764b2d2e7a5c0548897b4a2279774
Andrey Menshikh <andrey@light-tecnologies.com>
2017-06-18 21:53:44 +0300

    Request library

---
+++ client/src/scripts/api/requestHelper/request.js
@@ -0,0 +1,125 @@
+export const get = (path, successFunction, errorFunction) => {
+
+	const http = new XMLHttpRequest();
+
+	http.open("GET", getUrl() + "/" + path, true);
+
+	http.setRequestHeader("content-type", "application/json");
+
+	http.onreadystatechange = function () {
+		if (http.readyState === 4) {
+
+			if (http.status === 200) {
+				if (successFunction != undefined) {
+					successFunction(http.responseText);
+				}
+			} else {
+				if (errorFunction != undefined) {
+					errorFunction(http.responseText);
+				}
+
+				console.log('GET failed |', 'Path:', path, 'Message:', http.responseText);
+			}
+		}
+	}
+
+	http.send(null);
+}
+
+export const post = (path, data, successFunction, errorFunction) => {
+
+	const http = new XMLHttpRequest();
+
+	http.open("POST", getUrl() + "/" + path, true);
+
+	http.setRequestHeader("content-type", "application/json");
+
+	http.onreadystatechange = function () {
+		if (http.readyState === 4) {
+
+			if (http.status === 200) {
+				if (successFunction != undefined) {
+					successFunction(http.responseText);
+				}
+			} else {
+				if (errorFunction != undefined) {
+					errorFunction(http.responseText);
+				}
+
+				console.log('POST failed |', 'Path:', path, 'Message:', http.responseText);
+			}
+		}
+	}
+	http.send(JSON.stringify(data));
+}
+
+export const put = (path, id, data, successFunction, errorFunction) => {
+
+	const http = new XMLHttpRequest();
+
+	http.open("PUT", getUrl() + "/" + path + "/" + id, true);
+
+	http.setRequestHeader("content-type", "application/json");
+
+	http.onreadystatechange = function () {
+		if (http.readyState === 4) {
+
+			if (http.status === 200) {
+				if (successFunction != undefined) {
+					successFunction(http.responseText);
+				}
+			} else {
+				if (errorFunction != undefined) {
+					errorFunction(http.responseText);
+				}
+
+				console.log('PUT failed |', 'Path:', path, 'Id:', id, 'Message:', http.responseText);
+			}
+		}
+	}
+
+	http.send(JSON.stringify(data));
+}
+
+export const deleteRequest = (path, id, successFunction, errorFunction) => {
+
+	const http = new XMLHttpRequest();
+
+	http.open("DELETE", getUrl() + "/" + path + "/" + id, true);
+
+	http.onreadystatechange = function () {
+		if (http.readyState === 4) {
+
+			if (http.status === 200) {
+				if (successFunction != undefined) {
+					successFunction(http.responseText);
+				}
+			} else {
+				if (errorFunction != undefined) {
+					errorFunction(http.responseText);
+				}
+
+				console.log('PUT failed |', 'Path:', path, 'Id:', id, 'Message:', http.responseText);
+			}
+		}
+	}
+
+	http.send(null);
+}
+
+const getUrl = () => {
+	if (process.env.NODE_ENV === 'serverProduction') {
+		return 'http://iharryijumper.asuscomm.com:3000';
+	} else {
+		return 'http://localhost:3000';
+	}
+};
+
+const Request = {
+	get,
+	post,
+	put,
+	deleteRequest
+};
+
+export default Request;
